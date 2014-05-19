1681c394e7faae50494662c8badced11fa340f27
Philipp Burgmer <burgmer@weiglewilczek.com>
2014-05-20 00:29:08 +0200

    adds many slides and improves styling

--- src/app/app.js
+++ src/app/app.js
@@ -12,6 +12,37 @@ angular.module('app').constant('slidesConfig', {
     'about-me',
     'about-us',
 
+    'step-one/how-to-start',
+    'step-one/one-night-of-coding',
+    'step-one/too-many-files',
+    'step-one/3-month-project',
+    'step-one/problems',
+    'step-one/solution',
+
+    'step-one/bower-installation',
+    'step-one/bower-usage',
+
+    'step-one/grunt-installation',
+    'step-one/grunt-usage',
+    'step-one/grunt-example-livereload',
+    'step-one/grunt-example-include-js',
+    'step-one/jshint',
+
+    'step-two/more-problems',
+    'step-two/solution',
+    'step-two/still-more-to-do',
+    'step-two/the-real-problems',
+
+    'fabs/about',
+    'fabs/features',
+    'fabs/demo',
+
+    'perspective/gulp',
+    'perspective/broccoli',
+
+    'conclusion',
+
+    'meet-the-speaker',
     'end',
     'blank'
   ],
